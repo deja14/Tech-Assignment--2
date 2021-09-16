0d8858bfc87f432bd18ef2af89c0899d4674e61f
biven <biven@DESKTOP-C663FDP>
2021-09-16 00:27:17 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Tech_Assignment__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What Grade Do You Expect To Receive in ISM4300?");
+
+            try
+            {
+                string input = Console.ReadLine();
+                int Grade = int.Parse(input);
+
+                if (Grade < 60)
+                {
+                    Console.WriteLine ( "Your grade will be a F");
+                }
+                else if (Grade < 70)
+                {
+                    Console.WriteLine ("Your grade will be a D");
+                }
+                else if (Grade < 80)
+                {
+                    Console.WriteLine ("Your grade will be a C");
+                }
+                else if (Grade < 90)
+                {
+                    Console.WriteLine ("Your grade will be a B");
+                }
+                else
+                {
+                    Console.WriteLine ("Your grade will be a A");
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Press Any Key To Exit !");
+            }
+
+
+        }
+    }
+}
